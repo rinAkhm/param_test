2472f90bf0336049218d3da6f3efde2dd90a5581
rinAkhm <rin_akhm@bk.ru>
2021-08-30 15:31:29 +0300

    add dogsting

--- test_list_email.py
+++ test_list_email.py
@@ -2,7 +2,7 @@ import re
 import pytest
 
 
-def log(file_name, text):
+def log(file_name: str, text: str):
     with open(file_name, 'a', encoding='utf-8') as file:
         try:
             file.write(text + '\n')
@@ -10,7 +10,7 @@ def log(file_name, text):
             file.close()
 
 
-def check_to_valid_email(email:str) -> bool:
+def check_to_valid_email(email: str) -> bool:
     return bool(re.search(r'^[\w\.\+\-]+\@[ \w]+\.[a-z]{2,3}$', email['email']))
 
 
@@ -19,7 +19,7 @@ def check_to_valid_email(email:str) -> bool:
     {'email': 'w@w.com', 'expected': True},
     {'email': '123QWE@mmm.mmm', 'expected': True}
 ])
-def test_valid_email(data, file_path):
+def test_valid_email(data: dict, file_path: str):
     temp = check_to_valid_email(data)
     if temp is True:
         log(file_path, f'Результат {temp}: почта {data["email"]} валидная')
@@ -31,7 +31,7 @@ def test_valid_email(data, file_path):
     {'email': 'w@', 'expected': False},
     {'email': '@tt', 'expected': False}
 ])
-def test_invalid_email(data, file_path):
+def test_invalid_email(data: dict, file_path: str):
     temp = check_to_valid_email(data)
     if temp is False:
         log(file_path, f'Результат {temp}: почта {data["email"]} невалидная')
